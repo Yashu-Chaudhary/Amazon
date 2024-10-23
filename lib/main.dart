cc1149d1de06f8bf2c11cd8e5a6bf9714568964a
Yashu Chaudhary <yashusky232@gmail.com>
2024-10-23 15:05:41 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:amazon/constants/global_variables.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          iconTheme: IconThemeData(
+            color: Colors.black
+          ),
+        ),
+      ),
+      home: const Scaffold(body: Text('data')),
+    );
+  }
+}
